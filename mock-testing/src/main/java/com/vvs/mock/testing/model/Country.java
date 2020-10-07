c94eb07b34fece87ee6b25064a35b7dc82e1f8dc
adina-fometescu <adina.fometescu@elan-solutions.eu>
2020-10-07 15:56:44 +0300

    updated simple and mockito test

--- mock-testing/src/main/java/com/vvs/mock/testing/model/Country.java
+++ mock-testing/src/main/java/com/vvs/mock/testing/model/Country.java
@@ -1,10 +1,14 @@
 package com.vvs.mock.testing.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Country {
 
     private String name;
--- mock-testing/src/test/java/com/vvs/mock/testing/CapitalPrettyNameSimpleTest.java
+++ mock-testing/src/test/java/com/vvs/mock/testing/CapitalPrettyNameSimpleTest.java
@@ -1,43 +1,17 @@
 package com.vvs.mock.testing;
 
-import com.vvs.mock.testing.model.Country;
-import com.vvs.mock.testing.service.CountryClient;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-public class CapitalPrettyNameTest {
+public class CapitalPrettyNameSimpleTest {
 
-    @Mock
-    CountryClient countryClient;
 
-    private CapitalPrettyNameService capitalPrettyNameService;
-
-    @BeforeEach
-    public void init(){
-        this.capitalPrettyNameService = new CapitalPrettyNameService(countryClient);
-    }
-
-    private void mockCapital(String country, String capital) {
-        when(countryClient.findCountryWithName(country))
-                .thenReturn(Optional.of(Country.builder()
-                        .capital(capital)
-                        .name(country)
-                        .build()));
-    }
+    private CapitalPrettyNameService capitalPrettyNameService = new CapitalPrettyNameService(new CountryServiceMock());
 
     @Test
     public void whenCountryExists_thenReturnPrettyName() {
-        mockCapital("Romania", "Bucharest");
         String name = capitalPrettyNameService.getCapitalPrettyName("Romania");
         assertThat(name).isEqualTo("Bucharest(Romania)");
     }
@@ -50,14 +24,12 @@ public class CapitalPrettyNameTest {
 
     @Test
     public void whenCountryDoesNotHaveCapital_thenExpectNA() {
-        mockCapital("Nauru", "N/A");
         String name = capitalPrettyNameService.getCapitalPrettyName("Nauru");
         assertThat(name).isEqualTo("N/A(Nauru)");
     }
 
     @Test
     public void whenCountryHasMultipleCapitals_thenExpectAll() {
-        mockCapital("South Africa", "Cape Town, Bloemfontein, Pretoria");
         String name = capitalPrettyNameService.getCapitalPrettyName("South Africa");
         assertThat(name).contains("Cape Town");
         assertThat(name).contains("Bloemfontein");
